b9e31438348b6d5a2264e0f83344eee8d8d6e0ba
Mhd Sami Almouhtaseb <mssatnami@gmail.com>
2016-11-09 00:42:26 +0800

    update instance to connecto to eureka

--- src/main/java/com/insulter/web/InsulterWebApplication.java
+++ src/main/java/com/insulter/web/InsulterWebApplication.java
@@ -2,10 +2,16 @@ package com.insulter.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaClient
 @RestController
 public class InsulterWebApplication {
 
